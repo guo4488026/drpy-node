56670a27b174113b9e16f0fdbb37b86d22d3921e
guo <942541970@qq.com>
2025-05-11 16:06:08 +0800

    21

---
+++ js_dr2/红果短剧[短].js
@@ -0,0 +1,28 @@
+var rule = {
+  title: '红果短剧',
+  host: 'https://www.hongguodj.cc/',
+  url: '/show/fyclass--------fypage---.html',
+  searchUrl: '/search/**----------fypage---.html',
+  class_parse: '.nav li;a&&Text;a&&href;.*/(.*?).html',
+  searchable: 2,
+  quickSearch: 0,
+  filterable: 0,
+  headers: {
+    'User-Agent': 'MOBILE_UA',
+  },
+  play_parse: true,
+  lazy: "js:\n  let html = request(input);\n  let url = pdfh(html,'.video&&data-play');\n  input = {\n      parse: 0,\n      jx: 0,\n      url: url,\n    }",
+  limit: 6,
+  double: true,
+  推荐: '.show&&ul;li;img&&alt;img&&data-src;.bg&&Text;a&&href',
+  一级: '.list li;img&&alt;img&&data-src;.bg&&Text;a&&href',
+  二级: {
+    title: 'h2&&Text;.info p:eq(2)&&a&&Text',
+    img: 'img&&src',
+    desc: '.info p:eq(2)&&a&&Text;.info p:eq(3)&&a&&Text;.info p:eq(4)&&a&&Text;.info p:eq(0)&&a&&Text;.info p:eq(1)&&a&&Text',
+    content: '#desc&&Text',
+    tabs: '.play.my-2 .title&&a',
+    lists: '.play-list&&.rows:eq(#id)&&li',
+  },
+  搜索: '.show.rows li;img&&alt;img&&data-src;.bg&&Text;a&&href',
+}
