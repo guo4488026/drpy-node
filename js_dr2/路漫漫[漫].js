a40085fdbfe555704ee699facc76b85ad0a52292
guo4488026 <127454657+guo4488026@users.noreply.github.com>
2025-05-12 17:21:09 +0800

    更新 路漫漫[漫].js

--- js_dr2/路漫漫[漫].js
+++ js_dr2/路漫漫[漫].js
@@ -25,156 +25,165 @@ detailUrl: "/detail/fyid.html",
 	class_parse:'.container&&.tag.text-light;a&&Text;a&&href;.*/(.*?).html',
 	play_parse: true,
 	
-    lazy: $js.toString(() => {
-        function getDAesString(token) {
-            eval(getCryptoJS());
-            var key = CryptoJS.enc.Utf8.parse("ejjooopppqqqrwww");
+lazy: $js.toString(() => {
+   url = input;
 
-            var iv = CryptoJS.enc.Utf8.parse("1348987635684651");
-
-            var token = CryptoJS.AES.decrypt(token, key, {
-                iv: iv,
-                mode: CryptoJS.mode.CBC,
-                padding: CryptoJS.pad.Pkcs7,
-            });
-
-            return token.toString(CryptoJS.enc.Utf8);
-        }
-
-        var html = JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);
-        var url = html.url;
-        var from = html.from;
-        if (html.encrypt == "1") {
-            url = unescape(url);
-        } else if (html.encrypt == "2") {
-            url = unescape(base64Decode(url));
-        }
-        if (/\.mp4|\.m3u8|\.flv/.test(url)) {
-            input = {
-                parse: 0,
-                url: url.split("&")[0],
-                js: ''
-            };
+        let true_url = '';
+        var surl = url;
+        var host = HOST;
+        var html = request(url);
+        var play = {};
+        try {
+            play = JSON.parse(html.match(/r player_.*?=(.*?)</)[1]);
+        } catch (e) {}
+        //log(play)
+        var path = host + '/static/player/' + play.from + '.js';
+        var parse = fetch(path);
+        var match = /src = (.*?);/.exec(parse);
+        if (match) {
+            parse = match[1];
         } else {
-            var jsh = request(HOST + "/static/player/" + from + ".js", {
-                headers: {
-                    Referer: input
-                },
-            }).match(/\.src\s*=\s*(.*?);/)[1];
-            //log(MY_HOME);
-            if (/type=/.test(jsh)) {
-                jsh = jsh
-                    .replace(/[\+\s']/g, "")
-                    .replace(/MacPlayer.Parse/, "")
-                    .replace(/MacPlayer.PlayUrl/, url)
-                    .replace(/window.location.href/, input);
-                var playht = fetch(jsh, {
-                    headers: {
-                        Referer: HOST
-                    }
-                });
-            } else {
-                jsh = jsh
-                    .replace(/[\+\s']/g, "")
-                    .replace(/MacPlayer.Parse/, "")
-                    .replace(/MacPlayer.PlayUrl/, url)
-                    .replace(/window.location.href/, input);
-
-                jsh = JSON.parse(
-                    fetch(jsh, {
-                        headers: {
-                            Referer: HOST
-                        },
-                        onlyHeaders: true
-                    })
-                ).url;
-                var playht = fetch(jsh, {
-                    headers: {
-                        Referer: HOST
-                    }
-                });
-            }
-
-            var postapi = jsh.match(/^(.*?\/\/.*?\/.*?\/)/)[1];
-
-            var posturl = postapi + playht.match(/post\("(.*?)"/)[1];
-            if (/act\s*=/.test(playht)) {
-                var vid = playht.match(/vid\s*=\s*"(.*?)"/)[1];
-                var t = playht.match(/var\s*t\s*=\s*"(.*?)"/)[1];
-                var token = playht.match(/token\s*=\s*"(.*?)"/)[1];
-                var act = playht.match(/act\s*=\s*"(.*?)"/)[1];
-                var play = playht.match(/play\s*=\s*"(.*?)"/)[1];
-                token = getDAesString(token);
-
-                var data = JSON.parse(
-                    post(posturl, {
-                        headers: {
-                            Referer: HOST
-                        },
-                        body: {
-                            vid: vid,
-                            t: t,
-                            token: token,
-                            act: act,
-                            play: play,
-                        },
-                        timeout: 5000
-                    })
-                );
-                input = {
-                    parse: 0,
-                    url: data.url,
-                    js: ''
-                };
-            } else {
-                var key = "";
-
-                playht.match(/var (\w+)="(.*?)";/g).forEach(function(list) {
-                    key += list.match(/"(.*?)"/)[1];
-                });
-                const bodys = JSON.parse(
-                    playht
-                    .match(/post\(.*?,(.*?),\n/)[1]
-                    .replace(/"keyyy"\s*:\s*''.*?''/, '"keyyy" : "' + key + '"')
-                );
-                var data = JSON.parse(
-                    post(posturl, {
-                        headers: {
-                            Referer: HOST
-                        },
-                        body: bodys
-                    })
-                );
-                if (data.ext == "xgplayer") {
-                    var dataurl =
-                        "https://yun.366day.site/mp4hls/xgplayer.php?vid=" + data.url;
-                    var video = fetch(dataurl, {
-                        headers: {
-                            Referer: jsh
-                        }
-                    }).match(
-                        /"url": "(.*?)"/
-                    )[1];
-                    input = {
-                        parse: 0,
-                        url: video,
-                        js: ''
-                    };
-                } else if (data.ext == "hls" || data.ext == "hls_list") {
-                    input = {
-                        parse: 0,
-                        url: decodeURIComponent(data.url),
-                        js: ''
-                    };
-                } else {
-                    input = {
-                        parse: 0,
-                        url: data.url,
-                        js: ''
-                    };
-                }
+            parse = /src=(.*?)\+'/g.exec(parse)[1].replace(/^"/, "'");
+        }
+        
+        var window = {
+            location: {
+                href: surl
             }
         }
+        var MacPlayer = {
+            Parse: '',
+            PlayUrl: play.url,
+        }
+        if(/mp4|m3u8/.test(play.url)){
+            input = { parse: 0, url: play.url.replace(/&t=hls&ct=1|&t=hls/,"")  }
+        }
+
+        let trul = '';
+        eval('turl=' + parse);
+        let phtml = '';
+        var json = JSON.parse(fetch(turl, {
+            headers: {
+                Referer: surl,
+            },
+            onlyHeaders: true
+        }))
+        if (turl != json.url) {
+            phtml = fetch(json.url, {
+                headers: {
+                    Referer: turl,
+                },
+            });
+            turl = json.url;
+        } else {
+            phtml = json.body;
+        }
+      
+
+        try {
+            eval(phtml.match(/var vid = [\s\S]*?play.*;/g)[0]);
+        } catch {}
+        
+        try {
+            eval(phtml.match(/var weParse[\s\S]*?(?=function)/g)[0]);
+        } catch {}
+        
+        
+        eval(getCryptoJS());
+
+        function getc(_0xdab24c) {
+            var _0x32508f = gett('BlILGwo2OBoAATIXE1NXCwQALg0KE1xS');
+            var _0x32508f = decode3(_0x32508f);
+            var _0x145ef1 = '1348987635684651';
+            var _0x4e339a = getDAesString(_0xdab24c, _0x32508f, _0x145ef1);
+            return _0x4e339a;
+        }
+
+        function getDAesString(_0x4894f5, _0x94982e, _0x1a5246) {
+            var _0x94982e = CryptoJS.enc.Utf8.parse(_0x94982e);
+            var _0x1a5246 = CryptoJS.enc.Utf8.parse(_0x1a5246);
+            var _0x5e248d = CryptoJS.AES.decrypt(_0x4894f5, _0x94982e, {
+                'iv': _0x1a5246,
+                'mode': CryptoJS.mode.CBC,
+                'padding': CryptoJS.pad.Pkcs7
+            });
+            return _0x5e248d.toString(CryptoJS.enc.Utf8);
+        }
+
+        function base64decode2(_0x6288a9) {
+            var _0x129e5e = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+            var _0x135129, _0x2e0a45, _0x4ce1d8, _0xff4692, _0x2b9846, _0x596dac, _0x1e1130, _0x137328, _0x52e869 = 0,
+                _0x5997e8 = 0,
+                _0x51fd8d = '',
+                _0x408cf1 = [];
+            if (!_0x6288a9) {
+                return _0x6288a9;
+            }
+            _0x6288a9 += '';
+            do {
+                _0xff4692 = _0x129e5e.indexOf(_0x6288a9.charAt(_0x52e869++));
+                _0x2b9846 = _0x129e5e.indexOf(_0x6288a9.charAt(_0x52e869++));
+                _0x596dac = _0x129e5e.indexOf(_0x6288a9.charAt(_0x52e869++));
+                _0x1e1130 = _0x129e5e.indexOf(_0x6288a9.charAt(_0x52e869++));
+                _0x137328 = _0xff4692 << 0x12 | _0x2b9846 << 0xc | _0x596dac << 0x6 | _0x1e1130;
+                _0x135129 = _0x137328 >> 0x10 & 0xff;
+                _0x2e0a45 = _0x137328 >> 0x8 & 0xff;
+                _0x4ce1d8 = _0x137328 & 0xff;
+                if (_0x596dac == 64) {
+                    _0x408cf1[_0x5997e8++] = String.fromCharCode(_0x135129);
+                } else if (_0x1e1130 == 64) {
+                    _0x408cf1[_0x5997e8++] = String.fromCharCode(_0x135129, _0x2e0a45);
+                } else {
+                    _0x408cf1[_0x5997e8++] = String.fromCharCode(_0x135129, _0x2e0a45, _0x4ce1d8);
+                }
+            } while (_0x52e869 < _0x6288a9.length);
+            _0x51fd8d = _0x408cf1.join('');
+            return _0x51fd8d;
+        }
+
+        function gett(_0x55e7f2) {
+            key = 'daolianlaopowanrenlun';
+            _0x55e7f2 = base64decode2(_0x55e7f2);
+            len = key.length;
+            code = '';
+            for (i = 0; i < _0x55e7f2.length; i++) {
+                k = i % len;
+                code += String.fromCharCode(_0x55e7f2.charCodeAt(i) ^ key.charCodeAt(k));
+                ttcode = base64decode2(code);
+            }
+            return ttcode;
+        }
+
+        function decode3(input) {
+            var sortedString = input.split('').sort((char1, char2) => {
+                return char1.localeCompare(char2, 'zh-CN');
+            }).join('');
+            return sortedString;
+        }
+
+        var [_, u, body] = /\$\.post\("(.*?)",.?({.*?})/g.exec(phtml);
+
+        eval('body=' + body);
+        var php = "<a href='" + u + "'>";
+        var p = pd(php, 'a&&href', turl);
+        
+        var tbody = Object.keys(body).map(it=>{
+          return it+"="+body[it]
+        }).join("&")
+        log(tbody)
+        
+        var json = request(p, {
+            body: tbody,
+            method: "POST",
+            headers: {
+                Referer: turl,
+            }
+        })
+        try {
+            true_url = JSON.parse(json).url;
+            true_url = decodeURIComponent(true_url);
+        } catch {}
+        input = {parse:0,url: true_url}
     }),
 
 	limit: 6,
